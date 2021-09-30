7b9cfecdeecc6b2a765e2a9a1eb132eb9198e83f
Dmitry <example@gmail.com>
2021-09-30 16:02:43 +0300

    math

--- vic.js
+++ vic.js
@@ -1,10 +1,12 @@
 'use strict';
 
- let sum = prompt('Введите сумму зарплаты');
- function res (){
-	let res ;
-	res = sum*15%
- }
- alert(res());
+let sum = +prompt('Введите сумму зарплаты');
+let res = sum / 100 * 15;
+let nalog = (res+sum) / 100 * 10;
+alert('Премия 15%. На руки ' + (res+sum));
+alert('Налоги -10%. На руки ' + (res-nalog+sum));
+alert('В магазине -190. Осталось ' + ((res-nalog+sum)-190));
+alert('Жене половину отдал. Осталось ' + (((res-nalog+sum)-190))/2);
+
 	
 	
